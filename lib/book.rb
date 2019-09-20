dde52ad813c445b653b65e376af9e59b29f178d8
Richell Bonfield <richell.bonfield12@gmail.com>
2019-09-20 02:35:14 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,6 +1,6 @@
 class Book
   
-  attr_accessor :author, :page_count, :genre
+  attr_accessor :author, :page_count
   
   def initialize(title)
     @title = title
@@ -10,7 +10,13 @@ class Book
     @title
   end 
   
+  def genre=(genre)
+    @genre = genre
+  end
+ 
   def genre
+    @genre
+  end
  
   def turn_page
     puts "Flipping the page...wow, you read fast!"
